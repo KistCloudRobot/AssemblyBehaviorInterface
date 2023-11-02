d989873f5310204b5bd0601ce0be192e937ed83d
pc <pc@DESKTOP-VBRQNRK>
2023-11-02 15:18:24 +0900

    트레이 포트 반영

--- AssemblyBehaviorInterface/src/kr/ac/uos/ai/behavior/Configuration.java
+++ AssemblyBehaviorInterface/src/kr/ac/uos/ai/behavior/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration {
 	public static final String SERVER_ADDRESS = "127.0.0.1";
 	public static final int SERVER_PORT_EPSON = 61116;
 	public static final int SERVER_PORT_UR = 61115;
-	public static final int SERVER_PORT_PRODUCTION_FACILITY = 61114;
+	public static final int SERVER_PORT_PRODUCTION_FACILITY = 61316;
 	public static final BrokerType BROKER_TYPE = BrokerType.ACTIVEMQ;
 	
   	public static final int EPSON_ARM_PORT = 1470;
@@ -23,11 +23,11 @@ public class Configuration {
 	
 	public static final String LABEL_PRINTER_PORT = "COM4";
 
-	public static final String PCB_TRAY_PORT = "COM4";
-	public static final String HOUSING_TRAY_PORT = "COM4";
-	public static final String LENS_TRAY_PORT = "COM4";
-	public static final String FRONT_TRAY_PORT = "COM4";
-	public static final String SUB_PC_PORT = "COM??";
+	public static final String PCB_TRAY_PORT = "COM5";
+	public static final String HOUSING_TRAY_PORT = "COM6";
+	public static final String LENS_TRAY_PORT = "COM7";
+	public static final String FRONT_TRAY_PORT = "COM8";
+	public static final String SUB_PC_PORT = "COM9";
 	
 	public static final int BEATRATE = 115200;
 	
